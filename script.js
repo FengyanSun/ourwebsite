faa5faf371c18721b55baf0d36cf05a3ac3ef14d
FengyanSun <wellautogary@gmail.com>
2026-05-17 00:06:41 -0700

    update

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ let currentPage = 0;
 
 const pages = document.querySelectorAll(".page");
 
+const memoryLink = document.getElementById("memory-link");
+
 document.body.addEventListener("click", function () {
+
+    if (currentPage >= pages.length - 1) {
+        return;
+    }
+
     pages[currentPage].classList.remove("active");
 
     currentPage = currentPage + 1;
 
-    if (currentPage >= pages.length) {
-        currentPage = 0;
+    pages[currentPage].classList.add("active");
+
+    if (currentPage === pages.length - 1) {
+        memoryLink.style.display = "block";
     }
 
-    pages[currentPage].classList.add("active");
 });
